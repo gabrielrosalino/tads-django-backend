1379bfe090f8e1c823e605f499dcf7197e0f0f29
Lorena πacente <lorenapprog@gmail.com>
2025-05-17 18:49:45 -0300

    Finalização de Cadastro de Alunos

--- ad1/ad1/views.py
+++ ad1/ad1/views.py
@@ -6,8 +6,8 @@ def index(request):
 def dashboard (request):
     return render(request, 'dashboard.html')
 
-def cadastrar_aluno (request):
-    return render(request, 'cadastrar_aluno.html')
+def add_student (request):
+    return render(request, 'add_student.html')
 
 def configNotifications(request):
     return render(request, 'configNotifications.html')
