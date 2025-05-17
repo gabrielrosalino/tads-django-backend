3beb067576543c302a4627e0f52963396f11de9c
Lorena πacente <lorenapprog@gmail.com>
2025-05-17 16:46:57 -0300

    Adicionando o Cadastro de Alunos

--- ad1/ad1/views.py
+++ ad1/ad1/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def dashboard (request):
     return render(request, 'dashboard.html')
+def cadastrar_aluno (request):
+    return render(request, 'cadastrar_aluno.html')
