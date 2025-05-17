8e13f98cca408db92f2da0d0a22b9e58cf4a3d49
andfsilva <68829450+andfsilva@users.noreply.github.com>
2025-05-17 13:46:08 -0300

    Atualiza configNotifications.html para teste de conexão com o github.

--- ad1/ad1/views.py
+++ ad1/ad1/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def dashboard (request):
     return render(request, 'dashboard.html')
+
+def configNotifications(request):
+    return render(request, 'configNotifications.html')
