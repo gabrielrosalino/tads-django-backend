b75fa71bcd5758d675fdb3b62c023e4715d41126
Gabriel Rosalino <gabriel.frosalino@gmail.com>
2025-05-18 18:35:20 -0300

    fix: revisa todas as rotas dos templates criados

--- ad1/ad1/urls.py
+++ ad1/ad1/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.index, name = 'index'),
     path('dashboard/', views.dashboard, name = 'dashboard'),
     path('configNotifications/', views.configNotifications, name='configNotifications'),
     path('add_student/', views.add_student, name = 'add_student'),
