71152ab113ac769f4c3a831fd3c964d9180192f9
andfsilva <68829450+andfsilva@users.noreply.github.com>
2025-06-12 19:59:00 -0300

    Criação de cadastro

--- ad1/ad1/urls.py
+++ ad1/ad1/urls.py
@@ -1,12 +1,14 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('', views.home, name = 'home'),
     path('dashboard/', views.dashboard, name = 'dashboard'),
     path('configNotifications/', views.configNotifications, name='configNotifications'),
     path('add_student/', views.add_student, name = 'add_student'),
     path('login/', views.login, name = 'login'),
-    path("home/", views.home, name="home"),
+    path('home/', views.home, name="home"),
+    path('cadastros/', include('cadastros.urls')),
 ]
--- ad1/cadastros/models.py
+++ ad1/cadastros/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Voluntario(models.Model):
+    nome = models.CharField("nome do voluntário", max_length = 50)
+
+    def __str__(self):
+        return self.nome
+    
+
