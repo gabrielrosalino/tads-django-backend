6721f5f02e403257b6e9310d9ded5f1eb1fe8204
Lorena πacente <lorenapprog@gmail.com>
2025-06-15 00:29:12 -0300

    Ajustes no Menu, CSS e criação dos templates html
    Alterações feitas:
    Antes, tínhamos 3 blocos no base.html (title, content e menu), agora temos só 2: title e content.
    Removi aquele bloco específico de menu, e agora toda a parte de menu fica fixa direto no base.html.
    
    Modelei todos os urls, paths, views e já criei em templates os arquivos .html, organizando em pastas por seção (ex.: alunos, disciplinas, etc).
    
    Dentro de cada pasta tem o arquivo referente a cada um dos links do menu.
    
    Por enquanto, as páginas estão vazias, só com um <h1> de título para identificar. A ideia é só popular elas depois.
    Na entrega vamos deixar uma nota no README e avisar o professor que essas telas existem mas serão preenchidas ao longo do projeto, por enquanto são só "placeholder".
    
    Por que precisei fazer isso:
    Pra conseguir aplicar o "link ativo", foi necessário ter as views funcionando, passando as variáveis de contexto, e usar um {% if %} nos <a> do menu.
    Assim, quando uma URL carrega, a respectiva variável ativa uma classe CSS chamada link-ativo, que aplica o azul claro no menu.

--- ad1/cadastros/urls.py
+++ ad1/cadastros/urls.py
@@ -11,4 +11,24 @@ urlpatterns = [
     path('add_student/', views.add_student, name = 'add_student'),
     path('login/', views.login, name = 'login'),
     path('home/', views.home, name="home"),
+
+    # --------- Alunos ----------
+    path('alunos/matricular/', views.matricular_aluno, name='matricular_aluno'),
+    path('alunos/pesquisar/', views.pesquisar_aluno, name='pesquisar_aluno'),
+
+    # --------- Disciplina ----------
+    path('disciplinas/cadastrar/', views.cadastrar_disciplina, name='cadastrar_disciplina'),
+    path('disciplinas/pesquisar/', views.pesquisar_disciplina, name='pesquisar_disciplina'),
+
+    # --------- Período Letivo ----------    
+    path('periodos/cadastrar/', views.cadastrar_periodo, name='cadastrar_periodo'),
+    path('periodos/pesquisar/', views.pesquisar_periodo, name='pesquisar_periodo'),
+
+    # --------- Turmas ----------
+    path('turmas/cadastrar/', views.cadastrar_turma, name='cadastrar_turma'),
+    path('turmas/pesquisar/', views.pesquisar_turma, name='pesquisar_turma'),
+
+    # --------- Voluntários ----------
+    path('voluntarios/cadastrar/', views.cadastrar_voluntario, name='cadastrar_voluntario'),
+    path('voluntarios/pesquisar/', views.pesquisar_voluntario, name='pesquisar_voluntario'),
 ]
--- ad1/cadastros/views.py
+++ ad1/cadastros/views.py
@@ -16,6 +16,47 @@ def configNotifications(request):
 def login(request):
     return render(request, 'login.html')
 
-@login_required #para apernas usuarios autenticados acessem a home
+# @login_required #para apernas usuarios autenticados acessem a home
 def home(request):
     return render(request, "home.html")
+
+
+
+# --------- Alunos ----------
+def matricular_aluno(request):
+    return render(request, 'alunos/matricular_aluno.html', {'active_menu': 'alunos'})
+
+def pesquisar_aluno(request):
+    return render(request, 'alunos/pesquisar_aluno.html', {'active_menu': 'alunos'})
+
+
+# --------- Disciplina ----------
+def cadastrar_disciplina(request):
+    return render(request, 'disciplinas/cadastrar_disciplina.html', {'active_menu': 'disciplina'})
+
+def pesquisar_disciplina(request):
+    return render(request, 'disciplinas/pesquisar_disciplina.html', {'active_menu': 'disciplina'})
+
+
+# --------- Período Letivo ----------
+def cadastrar_periodo(request):
+    return render(request, 'periodos_letivos/cadastrar_periodo.html', {'active_menu': 'periodo'})
+
+def pesquisar_periodo(request):
+    return render(request, 'periodos_letivos/pesquisar_periodo.html', {'active_menu': 'periodo'})
+
+
+# --------- Turmas ----------
+def cadastrar_turma(request):
+    return render(request, 'turmas/cadastrar_turma.html', {'active_menu': 'turmas'})
+
+def pesquisar_turma(request):
+    return render(request, 'turmas/pesquisar_turma.html', {'active_menu': 'turmas'})
+
+
+# --------- Voluntários ----------
+def cadastrar_voluntario(request):
+    return render(request, 'voluntarios/cadastrar_voluntario.html', {'active_menu': 'voluntarios'})
+
+def pesquisar_voluntario(request):
+    return render(request, 'voluntarios/pesquisar_voluntario.html', {'active_menu': 'voluntarios'})
