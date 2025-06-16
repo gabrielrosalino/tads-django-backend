6f452d5b647c1cf99e461ac25c3d65731149b0bd
Gabriel Rosalino <gabriel.frosalino@gmail.com>
2025-06-16 15:07:51 -0300

    fix: adequa rota alunos/matricular ao padrão

--- ad1/cadastros/urls.py
+++ ad1/cadastros/urls.py
@@ -10,15 +10,15 @@ urlpatterns = [
     path('login/', views.login, name = 'login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
     path('home/', views.home, name="home"),
-    path('dashboard/', views.dashboard, name = 'dashboard'),
-    path('configNotifications/', views.configNotifications, name='configNotifications'),
-    path('add_student/', views.add_student, name = 'add_student'),
+    # path('dashboard/', views.dashboard, name = 'dashboard'),
+    # path('configNotifications/', views.configNotifications, name='configNotifications'),
+    # path('add_student/', views.add_student, name = 'add_student'),
     path('accounts/', include('django.contrib.auth.urls')),
     
     # --------- Alunos ----------
     #path('alunos/matricular/', views.matricular_aluno, name='matricular_aluno'),
     path('alunos/pesquisar/', views.pesquisar_aluno, name='pesquisar_aluno'),
-    path('matricular_aluno/', views.matricular_aluno, name = 'matricular_aluno'),
+    path('alunos/matricular/', views.matricular_aluno, name = 'matricular_aluno'),
 
     # --------- Disciplina ----------
     path('disciplinas/cadastrar/', views.cadastrar_disciplina, name='cadastrar_disciplina'),
